c3f708c61e2e2d1a8245c7f9314110e6300785ac
Ezequiel Arevalo <ezequiel.d.arevalo@gmail.com>
2021-09-30 15:58:45 -0300

    eslint testing settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,4 +33,17 @@ module.exports = {
       },
     },
   },
+  overrides: [
+    {
+      files: [
+        '**/*.test.ts',
+        '**/*.test.tsx',
+        '**/*.test.js',
+        '**/*.test.jsx',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
 };
